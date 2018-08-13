95e0571f5503d2df0054c904e03f7fe4bc61067f
Michael <michael.simaan@gmail.com>
2018-08-13 22:44:04 +0300

    Initial commit

---
+++ include/model.py
@@ -0,0 +1,56 @@
+import tensorflow as tf
+
+
+def model():
+    _IMAGE_SIZE = 32
+    _IMAGE_CHANNELS = 3
+    _NUM_CLASSES = 10
+    dropout = 0.25
+    n_classes = 10
+
+    with tf.name_scope('main_params'):
+        x = tf.placeholder(tf.float32, shape=[None, _IMAGE_SIZE * _IMAGE_SIZE * _IMAGE_CHANNELS], name='Input')
+        y = tf.placeholder(tf.float32, shape=[None, _NUM_CLASSES], name='Output')
+        x_image = tf.reshape(x, [-1, _IMAGE_SIZE, _IMAGE_SIZE, _IMAGE_CHANNELS], name='images')
+
+        global_step = tf.Variable(initial_value=0, trainable=False, name='global_step')
+        learning_rate = tf.placeholder(tf.float32, shape=[], name='learning_rate')
+
+    with tf.variable_scope('conv1') as scope:
+        # Convolution Layer with 32 filters and a kernel size of 5
+        conv1 = tf.layers.conv2d(x_image, 32, 5, activation=tf.nn.relu)
+        # Max Pooling (down-sampling) with strides of 2 and kernel size of 2
+        conv1 = tf.layers.max_pooling2d(conv1, 2, 2)
+
+    with tf.variable_scope('conv2') as scope:
+        # Convolution Layer with 64 filters and a kernel size of 3
+        conv2 = tf.layers.conv2d(conv1, 64, 3, activation=tf.nn.relu)
+        # Max Pooling (down-sampling) with strides of 2 and kernel size of 2
+        conv2 = tf.layers.max_pooling2d(conv2, 2, 2)
+
+    with tf.variable_scope('Flaten_and_dense') as scope:
+        # Flatten the data to a 1-D vector for the fully connected layer
+        fc1 = tf.contrib.layers.flatten(conv2)
+
+        # Fully connected layer (in tf contrib folder for now)
+        fc1 = tf.layers.dense(fc1, 1024)
+        # Apply Dropout (if is_training is False, dropout is not applied)
+        fc1 = tf.layers.dropout(fc1, rate=dropout)
+
+        # Output layer, class prediction
+        softmax = tf.layers.dense(fc1, n_classes)
+    y_pred_cls = tf.argmax(softmax, axis=1)
+    return x, y, softmax, y_pred_cls, global_step, learning_rate
+
+
+def lr(epoch):
+    learning_rate = 1e-3
+    if epoch > 80:
+        learning_rate *= 0.5e-3
+    elif epoch > 60:
+        learning_rate *= 1e-3
+    elif epoch > 40:
+        learning_rate *= 1e-2
+    elif epoch > 20:
+        learning_rate *= 1e-1
+    return learning_rate
